86bd58b56d9cc2f7b9cacaf1d1248d83272df71e
Junhyun <mnmthatisus@gmail.com>
2021-12-22 14:25:23 +0900

    title change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Helmet } from "react-helmet";
 import { Info, Navbar } from "./components";
 import Home from "./page/Home";
 
 function App() {
   return (
     <>
+    <Helmet>
+      <title>ㅁㄴ이르ㅏㅁㄴ으라ㅣㅁㄴ으리ㅏㅡㅁㄴ이ㅏㄹㅁ니ㅏ인ㅇ리ㅏㅡ</title>
+    </Helmet>
       <Navbar />
       <Info />
       <Home />
